6591e3b3f7be9599ac66baec08ee53db3fa7a0e9
Abdutahir. Koresh <abdutahir@outlook.com>
2016-05-24 16:49:40 +0800

    escape html label

--- crawler.py
+++ crawler.py
@@ -3,20 +3,21 @@ __author__ = 'xudongwu'
 import sys
 import urllib
 import json
+import cgi
 
 url_base = "http://dict-co.iciba.com/api/dictionary.php?key=50047AA12F0CBAF069B6052DC978EDAE&type=json&w="
 
 out = open(sys.argv[2], mode='w')
 for line in open(sys.argv[1]):
-    word = line;
-    result = json.load(urllib.urlopen(url_base + word))
+    word = line.translate(None, '\n')
+    result = json.loads(cgi.escape(urllib.urlopen(url_base + word).read()))
     if 'word_name' in result:
         word_name = result['word_name']
         definition = result['symbols'][0]
         pronunciation = definition['ph_am']
         parts = definition['parts']
 
-        out.write("Q: " + word )
+        out.write("Q: " + word + '\n' )
         if pronunciation is None:
             out.write(("A: " + word_name + "<br/>").encode('utf8'))
         else:
